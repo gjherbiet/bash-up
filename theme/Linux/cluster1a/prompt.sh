42aea6fcba3051bff2cea62e66231135fd0cd5fa
Guillaume-Jean Herbiet <guillaume-jean@herbiet.net>
2010-11-17 11:37:22 +0100

    Added prompt end symbol coloration based on last command return value.

--- theme/Linux/cluster1a/prompt.sh
+++ theme/Linux/cluster1a/prompt.sh
@@ -6,7 +6,7 @@ export CLICOLOR='1'
 #
 # Prompt template
 #
-export PS1='\[\033[0;36m\]\u\[\033[1;36m\]@\h\[\033[00m\]:\[\033[01;33m\]\W$(__git_ps1 "\[\033[0;36m\][%s]")\[\033[1;34m\]>\[\033[00m\] '
+export PS1='\[\033[0;36m\]\u\[\033[1;36m\]@\h\[\033[00m\]:\[\033[01;33m\]\W$(__git_ps1 "\[\033[0;36m\][%s]")$RETURN_COLOR>\[\033[00m\] '
 
 #
 # Put current directory in terminal window/tab title
--- theme/prompt.sh
+++ theme/prompt.sh
@@ -4,11 +4,11 @@
 export CLICOLOR='1'
 
 #
-# Prompt template
-#
-export PS1='\[\033[00m\]\u\[\033[1;0m\]@\h\[\033[00m\]:\[\033[01;33m\]\W$(__git_ps1 "\[\033[0;36m\][%s]")\[\033[1;34m\]>\[\033[00m\] '
-
-#
 # Put current directory in terminal window/tab title
 #
-export PROMPT_COMMAND='echo -ne "\033]0;`hostname -s`:`pwd -P`\007";'
+export PROMPT_COMMAND='if [[ $? -eq 0 ]]; then RETURN_COLOR=`echo -ne "\033[0;32m"`; else RETURN_COLOR=`echo -ne "\033[0;31m"`; fi; echo -ne "\033]0;`hostname -s`:`pwd -P`\007";'
+
+#
+# Prompt template
+#
+export PS1='\[\033[00m\]\u\[\033[1;0m\]@\h\[\033[00m\]:\[\033[01;33m\]\W$(__git_ps1 "\[\033[0;36m\][%s]")$RETURN_COLOR>\[\033[00m\] '
