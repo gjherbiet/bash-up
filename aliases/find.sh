1d5db9999063c23c6edba9e5dca79b9b157942f4
Guillaume-Jean Herbiet <guillaume-jean@herbiet.net>
2010-11-17 11:36:44 +0100

    Fixed always on coloration in grep commands.

--- aliases/find.sh
+++ aliases/find.sh
@@ -2,6 +2,6 @@
 # Find stuff
 #
 alias grep='grep --color=auto'
-alias fgrep='fgrep --color=always'
-alias egrep='egrep --color=always'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 alias fname='find . -name'
