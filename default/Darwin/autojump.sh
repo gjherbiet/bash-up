7246efb4885e02cfbda02c31edd947024572f92b
Guillaume-Jean Herbiet <guillaume-jean@herbiet.net>
2011-01-03 10:01:34 +0100

    Fixed autojump loading due to change in script location.

--- default/Darwin/autojump.sh
+++ default/Darwin/autojump.sh
@@ -1,4 +1,4 @@
 # Added by autojump install.sh
-if [ -f /usr/local/Cellar/autojump/12/etc/profile.d/autojump.bash ]; then
-	source /usr/local/Cellar/autojump/12/etc/profile.d/autojump.bash
+if [ -f /usr/local/etc/autojump.bash ]; then
+	source /usr/local/etc/autojump.bash
 fi
